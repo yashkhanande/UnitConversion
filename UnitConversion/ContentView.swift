a0a0e4f04e6b3b2db549af44ea182f39291be3b1
yash Khanande <yashkhanande7@gmail.com>
2025-08-12 08:17:57 +0530

    Initial Commit

---
+++ UnitConversion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UnitConversion
+//
+//  Created by Yash  Khanande on 12/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
